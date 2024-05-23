f58374ce12059a1c5894c37e6caae3c9cf83f3d4
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-05-23 08:40:01 +0300

    Day 3 - 1 настройка логов sql, оптимизация sql запросов

--- service/service/settings.py
+++ service/service/settings.py
@@ -138,3 +138,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# настройка логов sql запросов
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
