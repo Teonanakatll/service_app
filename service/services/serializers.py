77f976df41e7a440941500908f1987cbc6727409
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-05-23 11:35:31 +0300

    Day 4 - 1 вычисление поля в сериализаторе и получение того же результатом с помощю annotate, aggregate в переопределённой функции list ModelViewSet

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -16,9 +16,20 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     # добавляем поле в сериализатор значение которого служит поле email модели user связанной с моделью client (ForeignKey/OneToOne)
     email = serializers.CharField(source='client.user.email')
+    # переменная для вывода аннотированого поля
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, read_only=True)
+    # договор а соглашении имён SerializerMethodField ищет функцию с префиксом get_имя_переменной
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.price
+
+    # def get_price(self, instance):
+    #     return (instance.service.full_price -
+    #             instance.service.full_price * (instance.plan.discount_percent / 100))
 
     class Meta:
         model = Subscription
         # мы можем указать 2 типа полей: поля сериализатора и поля самой модели
         # указываем plan_id это скрытое поле в которам хранится id связанной модели
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -11,6 +11,45 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related('plan',
         # для модели client запрашиваем связанного user, и берём только поле company_name,
         # и поле email из связанной модели user (OneToOne)
-        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    )
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))\
+        .annotate(price=(F('service__full_price') -
+                         F('service__full_price') *
+                         # 100.00 чтобы получить в результате дробь
+                         (F('plan__discount_percent') / 100.00)))
     serializer_class = SubscriptionSerializer
+
+    # переопределяем метод list который отвечает за обработку запросса и формирования ответа клиенту
+    def list(self, request, *args, **kwargs):
+        # вызываем базовый фильтр кверисета
+        queryset = self.filter_queryset(self.get_queryset())
+        # вызываем базовый метод list
+        response = super().list(request, *args, **kwargs)
+
+        # присваиваем данные response зночению словаря с ключём result
+        response_data = {'result': response.data}
+
+        # присваиваем данный словарь словарю дата в response
+        response.data = response_data
+        # добавляем в словерь ещё один ключ total_amount c агригигированной суммой анотированных значений
+        # и методом get() берём значение, aggregate в любом случае добавляет 1 запросс
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
